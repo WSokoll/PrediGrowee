5be649d1dcb8a76b104e8117f21b2d0c352cd2c6
WSokoll <wladek.sokol01@gmail.com>
2022-12-22 19:07:52 +0100

    database + flask security setup

--- app/app.py
+++ app/app.py
@@ -1,7 +1,16 @@
+from datetime import datetime
+
 from flask import Flask
+from flask_mail import Mail
+from flask_security import SQLAlchemyUserDatastore, Security
+from flask_security.models import fsqla_v2
+from flask_sqlalchemy import SQLAlchemy
 from authlib.integrations.flask_client import OAuth
 
 
+db = SQLAlchemy()
+security = Security()
+mail = Mail()
 oauth = OAuth()
 
 
@@ -16,7 +25,32 @@ def create_app():
     app.config.from_pyfile('config.default.py')
     app.config.from_pyfile('../local/config.local.py')
 
+    app.config["SECURITY_DATETIME_FACTORY"] = datetime.now
+    app.config['SQLALCHEMY_DATABASE_URI'] = (f"mysql://{app.config['DB_USER']}:{app.config['DB_PASSWORD']}"
+                                             f"@{app.config['DB_HOST']}/{app.config['DB_NAME']}")
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.reflect()
+
+    from sqlalchemy import Column, Integer, ForeignKey
+    fsqla_v2.FsModels.db = db
+    fsqla_v2.FsModels.user_table_name = "user"
+    fsqla_v2.FsModels.role_table_name = "role"
+    fsqla_v2.FsModels.roles_users = db.Table(
+        "roles_users",
+        Column("user_id", Integer(), ForeignKey(f"user.id")),
+        Column("role_id", Integer(), ForeignKey(f"role.id")),
+        extend_existing=True
+    )
+
+    from app.models import User, Role
+    user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+
+    mail.init_app(app)
     oauth.init_app(app)
+    security.init_app(app, user_datastore)
 
     from app.views.welcome import bp as bp_welcome
     app.register_blueprint(bp_welcome)
