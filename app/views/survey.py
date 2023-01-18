99c1b48af115df3c2857a97be2fbd27de234d5da
WSokoll <wladek.sokol01@gmail.com>
2023-01-18 18:20:35 +0100

    survey endpoint - backend

--- app/views/survey.py
+++ app/views/survey.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from flask_login import current_user
 from flask_security import auth_required
 
+from app.app import db
 from app.forms.survey import SurveyForm
 from app.models import Survey
 
@@ -16,12 +17,23 @@ def get_post():
     survey = Survey.query.filter_by(user_id=current_user.id).first()
 
     if survey:
-        pass
-        # return redirect
+        return redirect(url_for('game.get_post'))
 
     form = SurveyForm()
 
     if form.validate_on_submit():
-        pass
+        survey = Survey(
+            user_id=current_user.id,
+            gender=form.gender.data,
+            age=form.age.data,
+            vision_defect=form.vision_defect.data,
+            education=form.education_other.data if form.education.data == 'other' else form.education.data,
+            experience=form.experience.data
+        )
+
+        db.session.add(survey)
+        db.session.commit()
+
+        return redirect(url_for('game.get_post'))
 
     return render_template('survey.jinja', form=form)
