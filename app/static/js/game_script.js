12fe831aa7907e4f29ae36feeb630274feb2633d
WSokoll <wladek.sokol01@gmail.com>
2023-02-12 12:57:40 +0100

    Game FRONTEND - script.js (sidebar and submission buttons)

---
+++ app/static/js/game_script.js
@@ -0,0 +1,22 @@
+
+$().ready(function() {
+    // On submit function - two different submit buttons
+    $('#prediction-form').on('submit', function () {
+         let screenSizeInput = document.getElementById('screen_size');
+         screenSizeInput.value = window.innerWidth.toString() + 'x' + window.innerHeight.toString();
+
+         let buttonId = $(document.activeElement).attr('id');
+         let showResultsInput = document.getElementById('show_results');
+         showResultsInput.value = buttonId === 'btn-results';
+
+         return true;
+    });
+});
+
+// Sidebar transition on/off
+function transitionOn(on) {
+    let offcanvas = document.getElementsByClassName('offcanvas');
+    for (let i = 0; i < offcanvas.length; i++) {
+        offcanvas[i].style['transition-duration'] = on ? '.3s' : '0s';
+    }
+}
