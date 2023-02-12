92d4b3a282b920f167c2c265729b5c18fc243224
WSokoll <wladek.sokol01@gmail.com>
2023-02-12 12:58:32 +0100

    Game BACKEND - game form with hidden fields

--- app/forms/game.py
+++ app/forms/game.py
@@ -1,11 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import RadioField, HiddenField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, Regexp
 
+# The first value of the tuple should indicate position on the list
 DIRECTION_CHOICES = [(0, 'predominantly horizontal'), (1, 'predominantly vertical'), (2, 'mixed')]
 
 
 class GameForm(FlaskForm):
     direction = RadioField('Direction of the facial growth', choices=DIRECTION_CHOICES, validators=[InputRequired()])
-    screen_size = HiddenField('Screen size')  # TODO validators
-    show_results = HiddenField('Show results')  # TODO validators
+    screen_size = HiddenField('Screen size')  # <-- validation done after form submission
+    show_results = HiddenField('Show results', validators=[
+        InputRequired('Please use dedicated buttons to submit the prediction form.'),
+        Regexp(r'^true$|^false$', message='Please use dedicated buttons to submit the prediction form.')
+    ])
