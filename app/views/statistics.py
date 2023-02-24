1de29e5b48ab478fda5c1d7d0448881f309d4535
WSokoll <wladek.sokol01@gmail.com>
2023-02-24 18:07:43 +0100

    Statistics BACKEND

--- app/views/statistics.py
+++ app/views/statistics.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template
+from flask_login import current_user
 from flask_security import auth_required
+
+from app.app import db
 
 bp = Blueprint('statistics', __name__)
 
@@ -7,5 +10,36 @@ bp = Blueprint('statistics', __name__)
 @bp.route('/statistics', methods=['GET'])
 @auth_required()
 def get():
-    # TODO overall statistics
-    return render_template('statistics.jinja')
+    statistics = {
+        'time-limited': {
+            'count_all': 0,
+            'count_correct': 0,
+            'sum_time': 0
+        },
+        'educational': {
+            'count_all': 0,
+            'count_correct': 0,
+            'sum_time': 0
+        },
+        'classic': {
+            'count_all': 0,
+            'count_correct': 0,
+            'sum_time': 0
+        }
+    }
+
+    for game_mode in ['classic', 'educational', 'time-limited']:
+        query = db.session.execute(
+            """
+            SELECT COALESCE(COUNT(id), 0) AS count_all, 
+                   COALESCE(SUM(answer_correct), 0) AS count_correct,
+                   COALESCE(SUM(time_spent), 0) AS sum_time 
+            FROM user_results 
+            WHERE user_id = :user_id AND game_mode = :game_mode
+            """,
+            {'user_id': current_user.id, 'game_mode': game_mode})
+
+        for key, value in query.mappings().all()[0].items():
+            statistics[game_mode][key] = value
+
+    return render_template('statistics.jinja', statistics=statistics)
