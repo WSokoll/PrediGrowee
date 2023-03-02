2b96c07c429eb564c687660cde62749f94d18390
WSokoll <wladek.sokol01@gmail.com>
2023-03-02 17:00:02 +0100

    Admin panel - home link

--- app/admin.py
+++ app/admin.py
@@ -5,10 +5,18 @@ from app import models
 from app.views.admin.draw_mode import DrawModeModelView
 
 
+# TODO add ort_parameters table view
 def admin_panel_init(admin, db):
+
     class LogoutLink(MenuLink):
         def get_url(self):
             return url_for("security.logout")
 
+    class HomePageLink(MenuLink):
+        def get_url(self):
+            return url_for("welcome.get")
+
+    admin.add_link(HomePageLink(name="Home"))
     admin.add_link(LogoutLink(name="Log out"))
+
     admin.add_view(DrawModeModelView(models.CaseGrouping, db.session, name='Draw Mode'))
