6db54297eb386ee655ecacc93a4e0f1bc1c15a5d
WSokoll <wladek.sokol01@gmail.com>
2023-06-01 09:36:40 +0200

    Admin panel update - results

--- app/views/admin/results.py
+++ app/views/admin/results.py
@@ -2,16 +2,18 @@ from flask_admin.contrib.sqla import ModelView
 
 
 class ResultsModelView(ModelView):
-    can_view_details = False
+    can_view_details = True
     can_create = False
     can_edit = False
     can_delete = True
 
     column_list = ['id', 'user_id', 'user.email']
     column_sortable_list = ['id', 'user_id', 'user.email']
+    column_details_list = ['id', 'user_id', 'user.email', 'screen_size']
 
     column_labels = {
         'id': 'ID',
         'user_id': 'User ID',
-        'user.email': 'User email'
+        'user.email': 'User email',
+        'screen_size': 'Screen size'
     }
