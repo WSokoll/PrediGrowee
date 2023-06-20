a0deb07469879231767debea37ae116be380f333
WSokoll <wladek.sokol01@gmail.com>
2023-06-20 10:15:03 +0200

    Adding second contact email - CONTACT subpage

--- app/views/contact.py
+++ app/views/contact.py
@@ -6,5 +6,6 @@ bp = Blueprint('contact', __name__)
 @bp.route('/contact', methods=['GET'])
 def get():
     contact_email = current_app.config['CONTACT_EMAIL']
+    contact_email_2 = current_app.config['CONTACT_EMAIL_2']
 
-    return render_template('contact.jinja', contact_email=contact_email)
+    return render_template('contact.jinja', contact_email=contact_email, contact_email_2=contact_email_2)
