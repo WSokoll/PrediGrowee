d41603c1b41c663bcca2705cd7ae11ac44c6350c
WSokoll <wladek.sokol01@gmail.com>
2023-06-20 10:09:53 +0200

    Admin panel - adding time spent - Results

--- app/views/admin/results.py
+++ app/views/admin/results.py
@@ -7,13 +7,14 @@ class ResultsModelView(ModelView):
     can_edit = False
     can_delete = True
 
-    column_list = ['id', 'user_id', 'user.email']
-    column_sortable_list = ['id', 'user_id', 'user.email']
-    column_details_list = ['id', 'user_id', 'user.email', 'screen_size']
+    column_list = ['id', 'user_id', 'user.email', 'time_spent']
+    column_sortable_list = ['id', 'user_id', 'user.email', 'time_spent']
+    column_details_list = ['id', 'user_id', 'user.email', 'screen_size', 'time_spent']
 
     column_labels = {
         'id': 'ID',
         'user_id': 'User ID',
         'user.email': 'User email',
+        'time_spent': 'Time spent [s]',
         'screen_size': 'Screen size'
     }
